a3ffc3f321c898b3760354fea5b916df9ffa137f
Mohamed Abdulmohsen <mabdelmohsen222@gmail.com>
2020-03-07 03:34:24 +0200

    add test to force refresh

--- app/src/test/java/com/me/daggersample/newsListing/NewsListingViewModelTest.kt
+++ app/src/test/java/com/me/daggersample/newsListing/NewsListingViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.me.daggersample.newsListing
 
+import android.opengl.Visibility
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
@@ -17,6 +18,7 @@ import com.me.daggersample.ui.SourcesListing.SourcesListingRepository
 import com.me.daggersample.ui.SourcesListing.SourcesListingViewModel
 import com.me.daggersample.validator.INetworkValidator
 import io.reactivex.Observable
+import io.reactivex.internal.util.ErrorMode
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -44,15 +46,15 @@ class NewsListingViewModelTest {
     fun `test no network when call sources list`() {
         Mockito.`when`(validator.isConnected()).then { false }
         sourceListingViewModel.getNewsListing()
-            .doOnSubscribe { `test do on subscribe first call to api`() }
-            .doOnError { `test do on error first call to api`() }
-            .subscribe({}, {})
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
         val value = sourceListingViewModel.errorLayoutVisibility.value
         val expected = ErrorModel(
-            message = R.string.no_network,
-            subMessage = R.string.please_try_again,
-            errorIcon = R.drawable.like,
-            visibility = VISIBLE
+                message = R.string.no_network,
+                subMessage = R.string.please_try_again,
+                errorIcon = R.drawable.like,
+                visibility = VISIBLE
         )
         Truth.assertThat(value).isEqualTo(expected)
         Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNull()
@@ -63,12 +65,12 @@ class NewsListingViewModelTest {
     fun `test server error when call sources list`() {
         Mockito.`when`(validator.isConnected()).then { true }
         Mockito.`when`(remoteDataSource.getTeamsList())
-            .then { Observable.just(ResponseStatus.ServerError("Hi iam in error mode now") as ResponseStatus<ApiResponse<Nothing>>) }
+                .then { Observable.just(ResponseStatus.ServerError("Hi iam in error mode now") as ResponseStatus<ApiResponse<Nothing>>) }
 
         sourceListingViewModel.getNewsListing()
-            .doOnSubscribe { `test do on subscribe first call to api`() }
-            .doOnError { `test do on error first call to api`() }
-            .subscribe({}, {})
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
 
         val value = sourceListingViewModel.errorLayoutVisibility.value
         val expected = ErrorModel(serverMessage = "Hi iam in error mode now", visibility = VISIBLE)
@@ -85,9 +87,9 @@ class NewsListingViewModelTest {
         }
 
         sourceListingViewModel.getNewsListing()
-            .doOnSubscribe { `test do on subscribe first call to api`() }
-            .doOnError { `test do on error first call to api`() }
-            .subscribe({}, {})
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
 
         val value = sourceListingViewModel.errorLayoutVisibility.value
         val expected = ErrorModel(visibility = VISIBLE)
@@ -101,22 +103,22 @@ class NewsListingViewModelTest {
         Mockito.`when`(validator.isConnected()).then { true }
         Mockito.`when`(remoteDataSource.getTeamsList()).then {
             Observable.just(
-                ResponseStatus.Error(
-                    message = R.string.something_went_wrong, subMessage = R.string.please_try_again
-                )
+                    ResponseStatus.Error(
+                            message = R.string.something_went_wrong, subMessage = R.string.please_try_again
+                    )
             )
         }
 
         sourceListingViewModel.getNewsListing()
-            .doOnSubscribe { `test do on subscribe first call to api`() }
-            .doOnError { `test do on error first call to api`() }
-            .subscribe({}, {})
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
 
         val value = sourceListingViewModel.errorLayoutVisibility.value
         val expected = ErrorModel(
-            message = R.string.something_went_wrong,
-            subMessage = R.string.please_try_again,
-            visibility = VISIBLE
+                message = R.string.something_went_wrong,
+                subMessage = R.string.please_try_again,
+                visibility = VISIBLE
         )
         Truth.assertThat(value).isEqualTo(expected)
         Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNull()
@@ -127,15 +129,15 @@ class NewsListingViewModelTest {
     fun `test correct data when call sources api`() {
         val jsonReader = `generate json reader`("src/test/res/sources_json_file")
         val sources =
-            Gson().fromJson<ApiResponse<ArrayList<Sources>>>(jsonReader, ApiResponse::class.java)
+                Gson().fromJson<ApiResponse<ArrayList<Sources>>>(jsonReader, ApiResponse::class.java)
         Mockito.`when`(validator.isConnected()).then { true }
         Mockito.`when`(remoteDataSource.getTeamsList()).then {
             Observable.just(ResponseStatus.Success(data = sources))
         }
         sourceListingViewModel.getNewsListing()
-            .doOnSubscribe { `test do on subscribe first call to api`() }
-            .doOnError { `test do on error first call to api`() }
-            .subscribe({}, {})
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
 
         val actual = sourceListingViewModel.sourcesListing.value
         val expected = sources.result
@@ -143,18 +145,140 @@ class NewsListingViewModelTest {
         Truth.assertThat(actual).isEqualTo(expected)
     }
 
+    @Test
+    fun `test no internet when api return data first time and force refresh`() {
+        val jsonReader = `generate json reader`("src/test/res/sources_json_file")
+        val sources =
+                Gson().fromJson<ApiResponse<ArrayList<Sources>>>(jsonReader, ApiResponse::class.java)
+        Mockito.`when`(validator.isConnected()).then { true }
+        Mockito.`when`(remoteDataSource.getTeamsList()).then {
+            Observable.just(ResponseStatus.Success(data = sources))
+        }
+        sourceListingViewModel.getNewsListing()
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
+
+
+        Mockito.`when`(validator.isConnected()).then { false }
+        sourceListingViewModel.getNewsListing(true)
+                .doOnSubscribe { `test do on subscribe force refresh with cashed data`() }
+                .doOnError { `test do on error force refresh with cashed data`() }
+                .subscribe({}, {})
+
+        val progressActualValue = sourceListingViewModel.refreshProgress.value
+        val progressExpectedValue = GONE
+
+        val errorMessageActualValue = sourceListingViewModel.errorMessage.value
+        val errorMessageExpectedValue =
+                ErrorModel(message = R.string.no_network, visibility = VISIBLE)
+
+        Truth.assertThat(progressActualValue).isEqualTo(progressExpectedValue)
+        Truth.assertThat(errorMessageActualValue).isEqualTo(errorMessageExpectedValue)
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotNull()
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotEmpty()
+        Mockito.verify(remoteDataSource, Mockito.times(1)).getTeamsList()
+    }
+
+    // TODO something wrong with server error and api failed
+
+    @Test
+    fun `test api failed when api return data first time and force refresh`() {
+        val jsonReader = `generate json reader`("src/test/res/sources_json_file")
+        val sources =
+                Gson().fromJson<ApiResponse<ArrayList<Sources>>>(jsonReader, ApiResponse::class.java)
+        Mockito.`when`(validator.isConnected()).then { true }
+        Mockito.`when`(remoteDataSource.getTeamsList()).then {
+            Observable.just(ResponseStatus.Success(data = sources))
+        }
+        sourceListingViewModel.getNewsListing()
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
+
+        Mockito.`when`(remoteDataSource.getTeamsList())
+                .then { ResponseStatus.ApiFailed(500, ErrorResponse(message = "Shit Server Failed :( !!")) }
+
+        sourceListingViewModel.getNewsListing()
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
+
+        val progressRefreshActualValue = sourceListingViewModel.refreshProgress.value
+        val progressRefreshExpectedValue = GONE
+//        Truth.assertThat(progressRefreshActualValue).isEqualTo(progressRefreshExpectedValue)
+
+        val errorMessageActualValue = sourceListingViewModel.errorMessage.value
+        val errorMessageExpectedValue = ErrorModel(serverMessage = "Shit Server Failed :( !!")
+//        Truth.assertThat(errorMessageActualValue).isEqualTo(errorMessageExpectedValue)
+
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotNull()
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotEmpty()
+    }
+
+    @Test
+    fun `test empty data when api return data first time and force refresh`() {
+        val jsonReader = `generate json reader`("src/test/res/sources_json_file")
+        val sources =
+                Gson().fromJson<ApiResponse<ArrayList<Sources>>>(jsonReader, ApiResponse::class.java)
+        Mockito.`when`(validator.isConnected()).then { true }
+        Mockito.`when`(remoteDataSource.getTeamsList()).then {
+            Observable.just(ResponseStatus.Success(data = sources))
+        }
+        sourceListingViewModel.getNewsListing()
+                .doOnSubscribe { `test do on subscribe first call to api`() }
+                .doOnError { `test do on error first call to api`() }
+                .subscribe({}, {})
+
+        val emptySources = ArrayList<Sources>()
+        val apiResponse = ApiResponse<ArrayList<Sources>>()
+        apiResponse.result = emptySources
+
+        Mockito.`when`(validator.isConnected()).then { true }
+        Mockito.`when`(remoteDataSource.getTeamsList())
+                .then { Observable.just(ResponseStatus.Success(data = apiResponse)) }
+        sourceListingViewModel.getNewsListing(true)
+                .doOnSubscribe { `test do on subscribe force refresh with cashed data`() }
+                .doOnError { `test do on error force refresh with cashed data`() }
+                .subscribe({}, {})
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotNull()
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isEmpty()
+        val listingActualValue = sourceListingViewModel.sourcesListing.value
+        Truth.assertThat(listingActualValue).isEqualTo(emptySources)
+        val errorLayoutActualValue = sourceListingViewModel.errorLayoutVisibility.value
+        val errorLayoutExpectedValue = ErrorModel(message = R.string.no_data, visibility = VISIBLE)
+        Truth.assertThat(errorLayoutActualValue).isEqualTo(errorLayoutExpectedValue)
+        Mockito.verify(remoteDataSource, Mockito.times(2)).getTeamsList()
+    }
+
+    private fun `test do on subscribe force refresh with cashed data`() {
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotNull()
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotEmpty()
+        Truth.assertThat(sourceListingViewModel.refreshProgress.value).isEqualTo(VISIBLE)
+        Truth.assertThat(sourceListingViewModel.errorLayoutVisibility.value)
+                .isEqualTo(ErrorModel(visibility = GONE))
+    }
+
+    private fun `test do on error force refresh with cashed data`() {
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotNull()
+        Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNotEmpty()
+        Truth.assertThat(sourceListingViewModel.refreshProgress.value).isEqualTo(GONE)
+        Truth.assertThat(sourceListingViewModel.errorLayoutVisibility.value)
+                .isEqualTo(ErrorModel(visibility = GONE))
+    }
+
     private fun `test do on subscribe first call to api`() {
         Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNull()
         Truth.assertThat(sourceListingViewModel.mainProgress.value).isEqualTo(VISIBLE)
         Truth.assertThat(sourceListingViewModel.errorLayoutVisibility.value)
-            .isEqualTo(ErrorModel(visibility = GONE))
+                .isEqualTo(ErrorModel(visibility = GONE))
     }
 
     private fun `test do on error first call to api`() {
         Truth.assertThat(sourceListingViewModel.testingCashedSourcesList).isNull()
         Truth.assertThat(sourceListingViewModel.mainProgress.value).isEqualTo(GONE)
         Truth.assertThat(sourceListingViewModel.errorLayoutVisibility.value)
-            .isEqualTo(ErrorModel(visibility = VISIBLE))
+                .isEqualTo(ErrorModel(visibility = VISIBLE))
     }
 
     private fun `generate json reader`(fileName: String): JsonReader {
