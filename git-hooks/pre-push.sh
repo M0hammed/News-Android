9fe75f507f2a64f4f372b52cacc141c67b915927
Mohamed Abdulmohsen <Mohamed.Abdulmohsen@integrant.com>
2021-02-24 16:51:04 +0200

    add pre-push git-hooks script

---
+++ git-hooks/pre-push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Running unit test 🥶🥶🥶"
+
+./gradlew testDevDebugUnitTest --daemon
+
+status=$?
+
+if [ "$status" = 0 ]; then
+  echo "💃💃💃 All unit test pass successfully. Processing with pushing the code"
+  exit 0
+else
+  echo 1>&2 "😤😤😤 Some test fails, Please check before pushing the code"
+  exit 1
+fi
